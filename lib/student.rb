0445f3d193c5942c3feb445202039064c0c4ad6e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 17:16:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,7 +66,7 @@ class Student
       LIMIT 1
     SQL
     
-     DB[:conn].execute(sql, 9).map do |row|
+    DB[:conn].execute(sql, 9).map do |row|
       self.new_from_db(row)
     end.first
   end
