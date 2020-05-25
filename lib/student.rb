bc6e2f44c172b9ad15757a85e8bafa6f7a0e618e
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 17:10:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,8 +7,7 @@ class Student
     new_song.id = row[0]
     new_song.name =  row[1]
     new_song.length = row[2]
-    new_song  # return the newly created instance
-  end
+    new_song  
 
   def self.all
     # retrieve all the rows from the "Students" database
