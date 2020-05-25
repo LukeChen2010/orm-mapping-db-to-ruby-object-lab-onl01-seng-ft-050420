8f44b9bf2264157ef0c74c3e06fe7feaf2d4efc0
Luke Chen <luke.chen.2010@gmail.com>
2020-05-25 17:20:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,7 +116,7 @@ class Student
     end.first
   end
   
-  def self.first_X_students_in_grade_10(x)
+  def self.first_students_in_grade_10(x)
     return self.first_X_students_in_grade_10(1)
   end
 end
